69163d330c13279c7739806f33061a090746cb29
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-05-02 16:17:36 +0530

    add

---
+++ E-Shopping/my-app/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+// import styled from "styled-components";
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav style={{ backgroundColor: "#8000ff", padding: 10 }}>
+         <div style={{display:"flex", justifyContent:"space-evenly", color:"white"}}>
+            <h4 style={{color:"black"}}>Home</h4>
+            <h4>About</h4>
+            <h4>Products</h4>
+            <h4>Mens</h4>
+            <h4>Womens</h4>
+            <h4>Kids</h4>
+            <h4>Home decor</h4>
+         </div>
+      </nav>
+      
+    </>
+    
+  );
+};
+
+export default Navbar;
